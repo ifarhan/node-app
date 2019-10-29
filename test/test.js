81f499073a1537cde5d24bd2c1ff51478779a1fc
root <root@localhost.localdomain>
2019-10-29 11:36:12 -0400

    index update

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ var request = require('supertest');
 var app = require('../index.js');
 
 describe('GET /', function(){
-	it('respond with hello world', function(done) {
-request(app).get('/').expect('hello world', done);
+	it('respond with hello again world', function(done) {
+request(app).get('/').expect('hello again world', done);
 });
 });
 
