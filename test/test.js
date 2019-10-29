60f5fe0e51093765c450eeaeb1c27e44548acb21
root <root@localhost.localdomain>
2019-10-29 08:11:44 -0400

    hello world app with tests

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function(){
+	it('respond with hello world', function(done) {
+request(app).get('/').expect('hello world', done);
+});
+});
+
