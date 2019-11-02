16a27c346d9211669a701e4f8bd010b369a5bbbd
Farhan Kalsekar <farhan.kalsekar@gmail.com>
2019-11-02 14:15:09 +1100

    remove unneccesary files

--- README.md
+++
@@ -1,3 +0,0 @@
-# node-app
-
-This is a test application for deploying a CI/CD pipeline using JENKINS
--- test/test.js
+++
@@ -1,9 +0,0 @@
-var request = require('supertest');
-var app = require('../index.js');
-
-describe('GET /', function(){
-	it('respond with hello again world', function(done) {
-request(app).get('/').expect('This is a test', done);
-});
-});
-
